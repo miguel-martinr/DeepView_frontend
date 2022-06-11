6ec66365837542ed12c3d19fce44d6aed656ca91
Miguel Martín <alu0101209777@ull.edu.es>
2022-06-11 19:05:03 +0100

    HomePage added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import { HomePage } from './pages/HomePage'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello world</h1>
+        <Routes>
+          <Route path='/home' element={<HomePage />} />
+        </Routes>
       </header>
     </div>
   )
