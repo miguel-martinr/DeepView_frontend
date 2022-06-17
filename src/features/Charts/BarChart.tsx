760d56652a2393c64445087b2be1bd7424e669a5
Miguel Martín <alu0101209777@ull.edu.es>
2022-06-17 19:22:19 +0100

    zoom plugin added

--- src/features/Charts/BarChart.tsx
+++ src/features/Charts/BarChart.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { CChartBar } from '@coreui/react-chartjs'
 import { CChartProps } from '@coreui/react-chartjs/dist/CChart'
 import Chart from 'chart.js/auto';
+import zoomPlugin from 'chartjs-plugin-zoom';
 import './styles.css'
 
 
@@ -14,6 +15,39 @@ interface BarChartProps extends
 
 export const BarChart = (props: BarChartProps) => {
   return (
-    <CChartBar {...props} className='chart'/>
+    <>
+      <CChartBar
+        {...props}
+        // className='chart'
+        plugins={[zoomPlugin]}
+        options={{
+          plugins: {
+            zoom: {
+              pan: {
+                enabled: true,
+                mode: 'xy',
+                threshold: 10,
+                
+              },
+              zoom: {
+                wheel: {
+                  enabled: true,
+                  speed: 0.1,
+                  
+                },
+                drag: {
+                  enabled: false,
+                  
+                },
+                pinch: {
+                  enabled: false
+                },
+                mode: 'xy',
+              }
+            }
+          }
+        }}
+      />
+    </>
   )
 }
