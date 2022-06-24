009a1651c042bec8de8791d0c653e12946a85e52
Miguel Martín <alu0101209777@ull.edu.es>
2022-06-24 15:36:40 +0100

    types fixed

--- src/state/workspace-slice.ts
+++ src/state/workspace-slice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { Video } from "../pages/VideosPage";
+import { Video } from "../types/Video";
 
 
 export interface VideoCollection {
